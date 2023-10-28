e56ba47038c8cfdd94c23d1f3bce73ec8f4c9a69
Carlos Reis <carlos.henriquer287@gmail.com>
2023-10-28 20:57:58 -0300

    layout

--- src/app/core/idea-form/idea-form.component.ts
+++ src/app/core/idea-form/idea-form.component.ts
@@ -1,30 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-idea-form',
   templateUrl: './idea-form.component.html',
   styleUrls: ['./idea-form.component.css'],
 })
-export class IdeaFormComponent {
+export class IdeaFormComponent implements OnInit {
+  name: string = '';
+  email: string = '';
+
   formGroup: FormGroup = new FormGroup({});
   isLoading: boolean = false;
   validationMessages: any = {
+    email: {
+      required: 'Campo obrigatório',
+    },
+    challenge: {
+      required: 'Campo obrigatório',
+    },
+    solution: {
+      required: 'Campo obrigatório',
+    },
+    gain: {
+      required: 'Campo obrigatório',
+    },
     passcode: {
       required: 'Campo obrigatório',
     },
   };
 
   formErrors: any = {
+    name: '',
+    email: '',
+    challenge: '',
+    solution: '',
+    gain: '',
     passcode: '',
   };
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private route: ActivatedRoute) {
     this.createForm();
+  }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(
+      (param: { name?: string; email?: string }) => {
+        console.log(param);
+        this.formGroup.patchValue({
+          name: param.name,
+          email: param.email,
+        });
+      }
+    );
   }
 
   createForm() {
     this.formGroup = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      challenge: ['', [Validators.required]],
+      solution: ['', [Validators.required]],
+      gain: ['', [Validators.required]],
       passcode: ['', [Validators.required]],
     });
 
